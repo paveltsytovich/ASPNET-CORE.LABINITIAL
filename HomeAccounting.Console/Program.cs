02b13e336656befe67e722781a4380f71c03f5a3
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2021-06-02 08:45:49 +0300

    Initial code

---
+++ HomeAccounting.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeAccounting.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
